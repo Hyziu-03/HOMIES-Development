1ef00199dcdbb7c49be3a66b6ef1dda0290c31df
Szymon Hyziak <szymon.hyziak@gmail.com>
2022-02-24 09:33:17 +0100

    Utilities' addition

--- src/App.js
+++ src/App.js
@@ -1,10 +1,8 @@
 import { Link } from 'react-router-dom';
-import Menu from './Menu';
 
 function App() {
   return (
     <article className='App'>
-      <Menu />
       <h1 className='heading'>Hi HOMIES!</h1>
       <Link to='/logowanie'>
         <button className='button'>Przejdź na stronę logowania</button>
--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import './styles/index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Login from './Login';
-import Zbieranina from './Zbieranina';
-import Calendar from './Calendar';
+import Login from './comps/Login';
+import Zbieranina from './comps/Zbieranina';
+import Latest from './comps/Latest';
 
 ReactDOM.render(
   <React.StrictMode>
@@ -15,7 +15,7 @@ ReactDOM.render(
         <Route path='/' element={<App />} />
         <Route path='/logowanie' element={<Login />} />
         <Route path='/zbieranina' element={<Zbieranina />} />
-        <Route path='/kalendarz' element={<Calendar />} />
+        <Route path='/ostatnie' element={<Latest />} />
       </Routes>
     </Router>
   </React.StrictMode>,
