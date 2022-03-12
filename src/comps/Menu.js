b9b373160b064aef8dbf26435cb803304642d8d8
Szymon Hyziak <szymon.hyziak@gmail.com>
2022-03-12 17:52:50 +0100

    Implementing design system

--- src/comps/Menu.js
+++ src/comps/Menu.js
@@ -1,11 +1,24 @@
 import { Link } from 'react-router-dom';
 
+import Home from '../svg/Home';
+import Calendar from '../svg/Calendar';
+import Profile from '../svg/Profile';
+
 const Menu = () => {
   return (
     <ol className='menu'>
 
-      <Link to='/' className='link' tabIndex='0'><li className='menu-item'>Ostatnie</li></Link>
-      <Link to='/zbieranina' className='link' tabIndex='0'><li className='menu-item'>Zbieranina</li></Link>
+      <Link to='/' className='link' tabIndex='0'>
+        <li className='menu-item'><Home/ ></li>
+      </Link>
+
+      <Link to='/zbieranina' className='link' tabIndex='0'>
+        <li className='menu-item'><Calendar /></li>
+      </Link>
+
+      <Link to='/ustawienia'>
+        <li className='menu-item'><Profile /></li>
+      </Link>
 
     </ol>
   );
---
+++ src/svg/Calendar.js
@@ -0,0 +1,7 @@
+const Calendar = () => {
+  return (
+    <svg width="45" height="44" viewBox="0 0 45 44" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M14.5 2V10M30.5 2V10M2.5 18H42.5M10.5 6.00001H34.5C38.9183 6.00001 42.5 9.58173 42.5 14V34C42.5 38.4183 38.9183 42 34.5 42H10.5C6.08172 42 2.5 38.4183 2.5 34V14C2.5 9.58173 6.08172 6.00001 10.5 6.00001Z" stroke="#ffffff" strokeWidth="3.33333" strokeLinecap="round" strokeLinejoin="round"/></svg>
+  );
+}
+
+export default Calendar;
