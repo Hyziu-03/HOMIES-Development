3d005552b75af137bafa227049c606abec883d69
Szymon Hyziak <szymon.hyziak@gmail.com>
2022-03-15 13:39:14 +0100

    Initialising Firebase

--- src/index.js
+++ src/index.js
@@ -13,6 +13,22 @@ import Privacy from './comps/Privacy';
 import Terms from './comps/Terms';
 import Settings from './comps/Settings';
 
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.8/firebase-app.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.6.8/firebase-analytics.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAhkZ7DS7BcnGHEwMkpOPc_7K8j8rW8b-A",
+  authDomain: "homies-web.firebaseapp.com",
+  projectId: "homies-web",
+  storageBucket: "homies-web.appspot.com",
+  messagingSenderId: "705309071530",
+  appId: "1:705309071530:web:e0a1832d656b4001e198d3",
+  measurementId: "G-ZXJ9YDRLZM"
+};
+
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
 ReactDOM.render(
   <React.StrictMode>
 
