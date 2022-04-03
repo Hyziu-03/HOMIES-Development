a82377038ea0931f5bf273ab2e42f9a8c830dca4
Szymon Hyziak <szymon.hyziak@gmail.com>
2022-04-03 15:39:01 +0200

    Fixing the errors of the auth flow

--- src/utils/modules.js
+++ src/utils/modules.js
@@ -1,21 +1,42 @@
-const validator = require("email-validator");
+// Node modules imports:
+
+const validator = require('email-validator');
 const passwordValidator = require('password-validator');
 
+// Name, email and passsword validation: 
+
 const schema = new passwordValidator();
-const passwordSchema = schema.is().min(9).has().letters().has().uppercase().has().lowercase().has().digits().has().symbols().has().not().spaces();
+
+schema
+    .is().min(9)
+
+    .has().letters()
+    .has().uppercase()
+    .has().lowercase()
+
+    .has().digits()
+    .has().symbols()
+
+    .has().not().spaces();
 
 export const validateName = (firstName, lastName) => !!firstName && !!lastName;
-export const validatePassword = (password) => passwordSchema.validate(password);
+
+export const validatePassword = (password) => schema.validate(password);
+
 export const validateEmail = (email) => validator.validate(email);
+
+// Integrating tabindex with the enter key:
 
 export const useTabIndex = () => {
 
     document.addEventListener('keydown', (event) => {
-
-        if (event.key === 13) {
-            document.activeElement.click();
+        try {
+            if (event.key === 13) {
+                document.activeElement.click();
+            }
+        } catch(error) {
+            console.error(error);
         }
-
     });
 
 }
