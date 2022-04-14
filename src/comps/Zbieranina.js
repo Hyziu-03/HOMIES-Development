6aee0e6a0c67c1275386105399027fe0c3109bc0
Szymon Hyziak <szymon.hyziak@gmail.com>
2022-04-14 17:54:32 +0200

    Implementing in-app browser

--- src/comps/Zbieranina.js
+++ src/comps/Zbieranina.js
@@ -10,14 +10,29 @@ import Menu from './Menu';
 
 const rejectAccess = () => !getAuth().currentUser ? console.warn('Zaloguj się, aby uzyskać uprawniony dostęp do Zbieraniny') : console.log();
   
+const toggleBrowser = () => setTimeout(() => document.getElementById('browser').style.display = 'block', 1000);
+
 const Zbieranina = () => {
 
   rejectAccess();
 
+  let url = 'https://www.matemaks.pl/';
+  
   return (
     <article className='zbieranina'>
+
       <Menu />
+
       <h1 className='subpage-heading'>Jesteś w Zbieraninie!</h1>
+
+      <iframe name='browser' id='browser' title='browser' className='browser'>
+        Twoja przeglądarka niestety nie obsługuje elementu ramki iframe.
+      </iframe>
+
+      <button className='button'>
+        <a href={url} target='browser' className='link' onClick={toggleBrowser}>Otwórz w ramce</a>
+      </button>
+
     </article>
   );
 
