3e0ac99aa9deff8961fb0b22a3c04082692ac281
Szymon Hyziak <szymon.hyziak@gmail.com>
2022-05-02 18:14:15 +0200

    Creating structure for Zbieranina

--- src/comps/Zbieranina.js
+++ src/comps/Zbieranina.js
@@ -7,9 +7,8 @@ import {
 // Component imports:
 
 import Menu from './Menu';
-import Angielski from '../img/Angielski';
-import Polski from '../img/Polski';
-// import Matematyka from '../img/Matematyka';
+import MockSubject from '../img/MockSubject';
+import MockElement from "../img/MockElement";
 
 const rejectAccess = () => !getAuth().currentUser ? console.warn('Zaloguj się, aby uzyskać uprawniony dostęp do Zbieraniny') : console.log();
 
@@ -22,25 +21,39 @@ const Zbieranina = () => {
 
       <Menu />
 
-      <h1 className='subpage-heading'><span className='greeting'>Cześć, XYZ!</span> <br/>   Czego chesz się dzisiaj nauczyć?</h1>
+      <h1 className='subpage-heading'><span className='greeting'>Cześć, XYZ!</span> <br/> Czego chesz się dzisiaj nauczyć?</h1>
 
       <input type='search' placeholder='Wyszukaj' className='search-bar'/>
 
-        <h2 className='subjects-heading'>Przedmioty</h2>
+      <h2 className='subjects-heading'>Przedmioty</h2>
         
-        <article className='subject-tiles'>
+      <article className='subject-tiles'>
 
-          <section className='subject-tile polski'>
-            <Polski />
-            <span className='subject-name'>Polski</span>
-          </section>
-          
-          <section className='subject-tile'>
-            <Angielski />
-            <span className='subject-name'>Angielski</span>
-          </section>
+        <section className='subject-tile'>
+          <MockSubject />
+          <hr className='subject-tile-break'/>
+          <span className='subject-name'>Język polski</span>
+        </section>
 
-        </article>
+        <section className='subject-tile'>
+          <MockSubject />
+          <hr className='subject-tile-break'/>
+          <span className='subject-name'>Język angielski</span>
+        </section>
+
+      </article>
+
+      <h2 className="subjects-heading">Ostatnie lekcje</h2>
+
+      <article className="last-lessons">
+        <MockElement />
+        <span className="lesson-name">Młoda Polska</span>
+      </article>
+
+      <article className="last-lessons">
+        <MockElement />
+        <span className="lesson-name">Wesele Wyspiańskiego</span>
+      </article>
 
     </article>
   );
