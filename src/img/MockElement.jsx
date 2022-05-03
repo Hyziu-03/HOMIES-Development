0266e937c1bd2db7559c822ef39262f95c92f851
Szymon Hyziak <szymon.hyziak@gmail.com>
2022-05-03 18:55:45 +0200

    Organising code into components

--- src/img/MockElement.jsx
+++
@@ -1,3 +0,0 @@
-const MockElement = () => <img src='https://picsum.photos/65' alt='' className='lesson-image'/>
-
-export default MockElement;
--- src/img/MockSubject.jsx
+++
@@ -1,3 +0,0 @@
-const MockSubject = () => <img src='https://picsum.photos/150' alt='' className='subject-image'/>;
-
-export default MockSubject;
