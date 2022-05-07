460c56ab29b65a0c27084b215c48da16c451a76c
Szymon Hyziak <szymon.hyziak@gmail.com>
2022-05-07 16:04:43 +0200

    Implementing Zbieranina

--- src/comps/SmallTile.jsx
+++ src/comps/SmallTile.jsx
@@ -1,11 +1,15 @@
-import MockImage from "./MockImage";
+import { Link } from 'react-router-dom';
+import MockImage from './MockImage';
+import { getCurrentPathName } from '../utils/modules';
 
 const SmallTile = (props) => {
   return (
-    <article className="small-tile">
+    <Link to={getCurrentPathName()} className='link'>
+      <article className='small-tile'>
         <MockImage size='65/65' class='small-tile-image'/>
-        <span className="lesson-name">{props.name}</span>
-    </article>
+        <span className='lesson-name'>{props.name}</span>
+      </article>
+    </Link>
   );
 }
 
--- src/pages/Documents.jsx
+++ src/pages/Documents.jsx
@@ -1,45 +1,26 @@
-import Menu from "../comps/Menu";
-import MockImage from "../comps/MockImage";
-import SearchBar from "../comps/SearchBar";
-import BackIcon from "../svg/BackIcon";
+import Menu from '../comps/Menu';
+import Document from '../comps/Document';
+import SearchBar from '../comps/SearchBar';
+import BackIcon from '../svg/BackIcon';
 
 const Documents = () => {
-  return (
-    <section className="documents">
-        <BackIcon />
-        <h1 className="documents-heading">Młoda Polska</h1>
-        <SearchBar />
-
-        <section className="documents-container">
-            <section className="item">
-            <MockImage size='155/110' class='documents-image'/>
-            <section className="item-info">
-                <span className="item-name">Kanon lektur - notatka</span>
-                <span className="tag">do 10 minut</span>
-                <span className="tag">notatka</span>    
-            </section>
-            </section>
-            <section className="item">
-            <MockImage size='155/110' class='documents-image'/>
-            <section className="item-info">
-                <span className="item-name">Kanon lektur - notatka</span>
-                <span className="tag">do 10 minut</span>
-                <span className="tag">notatka</span>    
-            </section>
-            </section>
-            <section className="item">
-            <MockImage size='155/110' class='documents-image'/>
-            <section className="item-info">
-                <span className="item-name">Kanon lektur - notatka</span>
-                <span className="tag">do 10 minut</span>
-                <span className="tag">notatka</span>    
-            </section>
-            </section>
-        </section>
-
-        <Menu />
-    </section>
-  );
+	return (
+		<section className='documents'>
+				<BackIcon />
+				<h1 className='documents-heading'>Młoda Polska</h1>
+				<SearchBar />
+				<section className='documents-container'>
+						<Document name='Materiał 1' tags='notatka, do 10 minut'/>
+						<Document name='Materiał 2' tags='artykuł, do 20 minut'/>
+						<Document name='Materiał 3' tags='podkast, do 60 minut'/>
+						<Document name='Materiał 4' tags='podkast, do 90 minut'/>
+						<Document name='Materiał 5' tags='notatka, do 30 minut'/>
+						<Document name='Materiał 6' tags='film, do 180 minut'/>
+						<Document name='Materiał 7' tags='serial, do 360 minut'/>
+				</section>
+				<Menu />
+		</section>
+	);
 }
 
 export default Documents;
